4e5101411230df11979c7dec770c2c60394cecea
Gabriel Sodre <gabriel.sodre@qintess.com>
2024-03-05 14:41:50 -0300

    Project structure - Path Mapping

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@theme": "./src/theme",
+            "@utils": "./src/utils",
+            "@assets": "./src/assets",
+            "@routes": "./src/routes",
+            "@screens": "./src/screens",
+            "@storage": "./src/storage",
+            "@components": "./src/components",
+          },
+        },
+      ],
+    ],
   };
 };
