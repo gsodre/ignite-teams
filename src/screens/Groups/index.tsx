bd7abe8868b2631f54efb23a0e1dec218695ea3c
Gabriel Sodre <gabriel.sodre@qintess.com>
2024-03-06 17:00:34 -0300

    Listing groups.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,15 +2,23 @@ import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { GroupCard } from "@components/GroupCard";
 import { Highlight } from "@components/Highlight";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Gurizada Medonha"]);
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
