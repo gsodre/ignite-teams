c4fb8e429536e744e604927aeb94609750933260
Gabriel Sodre <gabriel.sodre@qintess.com>
2024-03-06 01:03:34 -0300

    CSS-in-TS - Styling the StatusBar

--- App.tsx
+++ App.tsx
@@ -8,12 +8,18 @@ import {
 import { Groups } from "@screens/Groups";
 import theme from "@theme/index";
 import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        translucent
+        barStyle="light-content"
+        backgroundColor="transparent"
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
