a2fddb5782183253bd5e976f5f01f451e1a409b8
Gabriel Sodre <gabriel.sodre@qintess.com>
2024-03-07 14:26:59 -0300

    Creating Button Component.

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,26 @@
+import { UsersThree } from "phosphor-react-native";
+import { TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+
+export type ButtonTypeStyleProps = "PRIMARY" | "SECONDARY";
+
+type Props = {
+  type: ButtonTypeStyleProps;
+};
+
+export const Container = styled(TouchableOpacity)<Props>`
+  flex: 1;
+  min-height: 56px;
+  max-height: 56px;
+  border-radius: 6px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme, type }) =>
+    type === "PRIMARY" ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+`;
