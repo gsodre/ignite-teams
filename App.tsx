c28013002bb29f49ca359c3422a731fda6e5ef13
Gabriel Sodre <gabriel.sodre@qintess.com>
2024-03-09 00:30:47 -0300

    Navigation Structure.

--- App.tsx
+++ App.tsx
@@ -5,12 +5,10 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
-import { Groups } from "@screens/Groups";
 import theme from "@theme/index";
-import { Loading } from "@components/Loading";
+import { Routes } from "@routes/index";
 import { StatusBar } from "react-native";
-import { NewGroup } from "@screens/NewGroup";
-import { Players } from "@screens/Players";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -22,9 +20,7 @@ export default function App() {
         barStyle="light-content"
         backgroundColor="transparent"
       />
-      {/* {fontsLoaded ? <Groups /> : <Loading />} */}
-      {/* {fontsLoaded ? <NewGroup /> : <Loading />} */}
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
