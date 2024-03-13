09654224298ad569c608fc6d32682b4562edefc2
Gabriel Sodre <gabriel.sodre@qintess.com>
2024-03-13 00:18:19 -0300

    Local Storage - Handling exceptions.

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,25 +1,36 @@
 import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 import { Input } from "@components/Input";
+import { AppError } from "@utils/AppError";
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-import { useNavigation } from "@react-navigation/native";
 import { groupCreate } from "@storage/group/groupCreate";
 
 import { Container, Content, Icon } from "./styles";
+import { Alert } from "react-native";
 
 export function NewGroup() {
   const [group, setGroup] = useState("");
 
   const navigation = useNavigation();
 
-  async function handleNewPlayer() {
+  async function handleNewGroup() {
     try {
+      if (!group.trim().length) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma!");
+      }
+
       await groupCreate(group);
       navigation.navigate("players", { group });
     } catch (error) {
-      console.log("*** error", error);
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo!");
+        console.log("*** error", error);
+      }
     }
   }
 
@@ -40,7 +51,7 @@ export function NewGroup() {
       <Button
         title="Criar"
         style={{ marginTop: 20 }}
-        onPress={handleNewPlayer}
+        onPress={handleNewGroup}
       />
     </Container>
   );
